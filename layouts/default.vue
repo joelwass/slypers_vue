37855591f3b4dbe83bcd6e33a70b8cb30abc50ee
Joel Wasserman <joel.wasserman8@gmail.com>
2018-10-22 20:22:50 -0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -77,7 +77,7 @@ export default {
 }
 
 html {
-  font-family: 'OverpassMono-Bold', monospace;
+  font-family: 'OverpassMono-Regular', monospace;
   font-size: 13px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
